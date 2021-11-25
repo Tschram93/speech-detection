434d4c11282952323200e47ac3a104f1bec66209
TS93 <tschram93@yahoo.com>
2021-11-25 10:58:56 -0500

    cleaning code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Selectors
+const words = document.querySelector('.words');
+
+// Establishing new variables
+let p = document.createElement('p'); //Creates new `p` tags in html
+const recognition = new SpeechRecognition();
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// uses and functions
+recognition.interimResults = true;
+
+
+
+words.appendChild(p);
