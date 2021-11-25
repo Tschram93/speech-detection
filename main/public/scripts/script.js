9225b04999ff7f58761d759f78d421ee4491fc49
TS93 <tschram93@yahoo.com>
2021-11-25 12:28:34 -0500

    app working

--- main/public/scripts/script.js
+++ main/public/scripts/script.js
@@ -9,18 +9,29 @@ const recognition = new SpeechRecognition();
 
 // uses and functions
 recognition.interimResults = true;
-recognition.continuous = true;
+
+// recognition.continuous = true;
 words.appendChild(p);
 
 
 // Event Listeners
+
+// Start Event
 recognition.addEventListener('result', (e) => {
-    console.log(e.results);
     const transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
+        .join('');
+
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
 
     console.log(transcript);
 
 });
+// End Event 
+recognition.addEventListener('end', recognition.start);
 recognition.start();
