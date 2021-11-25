9505bbfdf065527b2091537c4caf029613313d45
TS93 <tschram93@yahoo.com>
2021-11-25 12:15:25 -0500

    function to turn results into an array from prototype;

--- main/public/scripts/script.js
+++ main/public/scripts/script.js
@@ -3,17 +3,24 @@ const words = document.querySelector('.words');
 
 // Establishing new variables
 let p = document.createElement('p'); //Creates new `p` tags in html
-const recognition = new SpeechRecognition();
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
 // uses and functions
 recognition.interimResults = true;
+recognition.continuous = true;
 words.appendChild(p);
 
 
 // Event Listeners
-recognition.addEventListener('results', (e) => {
-    console.log(e);
+recognition.addEventListener('result', (e) => {
+    console.log(e.results);
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+
+    console.log(transcript);
 
 });
+recognition.start();
